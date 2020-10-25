66471eb4b50d1af646e2c1997b4111de2f7b1215
melissadianne <melissa.schuster@bryanuniversity.edu>
2020-10-24 22:26:14 -0500

    pushtwo

---
+++ week5/submit_events_modifying_elements_value/index.js
@@ -0,0 +1,9 @@
+
+function getdetails() {
+  var a = document.forms["details"]["one"].value
+  var b = document.forms["details"]["two"].value
+  var c = document.forms["details"]["three"].value
+  
+    alert("You are "+a+" "+b+".\n Your favorite color is "+c);
+  }
+
---
+++ week5/submit_events_other_types_HTML/index.js
@@ -0,0 +1,80 @@
+//BODY STYLING
+var box = document.getElementById('box');
+box.style.backgroundImage = "url('https://images.unsplash.com/photo-1489212855151-30b04f553971?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80')";
+box.style.border = "solid white"
+box.style.opacity = "0.85";
+box.style.borderWidth = "50px";
+box.style.margin = "100px 200px";
+box.style.padding = "0";
+box.style.alignItems = "center";
+box.style.fontFamily = "georgia";
+
+//PERSON STYLING
+var person = document.getElementById('person');
+person.style.backgroundColor = "white"
+person.style.paddingTop = "10px"
+person.style.border = "solid white"
+;
+
+// //GENDER STYLING
+var genderselect = document.getElementById("genderselect")
+genderselect.style.backgroundColor = "white"
+genderselect.style.border = "solid white"
+;
+
+
+// //DESTINATION STYLING
+var whereto = document.getElementById("whereto")
+whereto.style.backgroundColor = "white"
+whereto.style.paddingBottom =  "50px"
+whereto.style.border = "solid white"
+;
+
+
+// //ALLERGINS STYLING
+var allergins = document.getElementById("allergins")
+allergins.style.backgroundColor = "white"
+allergins.style.padding = "10px"
+allergins.style.border = "solid white";
+
+
+
+
+ function display(){
+
+   var a = document.myForm.fname.value;
+   var b = document.myForm.lname.value;
+   var c = document.myForm.age.value;
+   var d = document.myForm.gender.value;
+   var e = document.myForm.destination.value;
+  
+   var items =document.getElementsByName("restriction");
+   console.log(document.getElementsByName("restriction"))
+   var food = ""
+   for(var i = 0; i< document.getElementsByName("restriction").length; i++){
+       console.log(document.getElementsByName("restriction")[i].checked)
+       if(document.getElementsByName("restriction")[i].checked){
+  
+            food+=document.getElementsByName("restriction")[i].value+" "
+       }
+   }
+   console.log(food)
+   alert("First name: "+a+"\n "+"Last name: "+b+"\n  "+"Age: "+c+" \n"+"Gender: "+d+" \n "+"Location: "+e+"\n "+"restriction: "+food); 
+   document.myForm.fname.value=""
+   document.myForm.lname.value=""
+   document.myForm.age.value=""
+   document.myForm.gender.value=""
+   ocument.myForm.destination.value=""
+   }
+  
+
+
+   const form = document.myForm
+  
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+       display()
+       form.reset()
+    })
+  
+    
